d518ac5d5721275e432add9d6422d4a67e7be687
domingocapelo <82053397+domingocapelo@users.noreply.github.com>
2021-05-17 21:23:04 +0200

    Add files via upload

--- AA-2/cargar_imagen.py
+++ AA-2/cargar_imagen.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 from Listas import List_name_files, List_Array_RGB
+import threading
 
 
 # Las imagenes no tienen el mismo tamano, por lo que para trabajar con ellas las redimensionaremos todas con el mismo tamano, en este caso elegimos redimensionarlas como : 64x64
@@ -12,17 +13,28 @@ from Listas import List_name_files, List_Array_RGB
 #  D1: Dimension 1 en pixeles
 #  D2: Dimension 2 en pixeles
 
-def Redim(pathImage,pathSave,D1,D2):
+def Redim(pathImage, pathSave, D1, D2):
     try:
         for item in os.listdir(pathImage):
             path = pathImage + "/" + str(item)
             img = Image.open(path)
             new_imag = img.resize((D1, D2))
-            pathsave_=pathSave + "/" + str(item)
+            pathsave_ = pathSave + "/" + str(item)
             new_imag.save(pathsave_)
     except:
         print("Error in funtion : Redim(pathImage,pathSave,listnames,D1,D2):")
 
+
+def Redim_Hilo(pathImage, l_name, pathSave, D1, D2):
+    try:
+        for i in range(len(l_name)):  # Numero de splits
+            path = pathImage + "/" + str(l_name[i])
+            img = Image.open(path)
+            new_imag = img.resize((D1, D2))
+            pathsave_ = pathSave + "/" + str(l_name[i])
+            new_imag.save(pathsave_)
+    except:
+        print("Error in Funtion Redim_Hilo(pathImage, l_name, pathSave, D1, D2) in cargar_imagen.py")
 
 # Funcion: Plot_Imagenes(StrTitulo,ListaTitulo,ListaImg,NumImg):
 #  Grafica n imagenes con sus respectivos titulos
@@ -31,42 +43,45 @@ def Redim(pathImage,pathSave,D1,D2):
 #  ListaImg: Lista de imagenes a imprimir
 #  NumImg : Numero de graficas a mostrar
 
-def Plot_Imagenes(StrTitulo,ListaTitulo,ListaImg,NumImg):
-    _, axes = plt.subplots(nrows=1, ncols=NumImg, figsize=(12, 8))  # Crea una figura con 1x5 subplots
-    i=0
-    for ax in axes: # Imprimos las 5 primeras imagen
+
+def Plot_Imagenes(StrTitulo, ListaTitulo, ListaImg, NumImg):
+    # Crea una figura con 1x5 subplots
+    _, axes = plt.subplots(nrows=1, ncols=NumImg, figsize=(12, 8))
+    i = 0
+    for ax in axes:  # Imprimos las 5 primeras imagen
         ax.set_axis_off()
-        #Titulo L_nameTrainImag
-        titulo=str(StrTitulo)+str(ListaTitulo[i])
+        # Titulo L_nameTrainImag
+        titulo = str(StrTitulo)+str(ListaTitulo[i])
         ax.set_title(titulo)
-        #Array imagen L_RGBTrain
-        ax.imshow(ListaImg[i],cmap=plt.cm.gray_r)
-        i+=1
+        # Array imagen L_RGBTrain
+        ax.imshow(ListaImg[i], cmap=plt.cm.gray_r)
+        i += 1
     plt.show(block=True)
+
 
 def cargar_imagenes(origen, destino, resolucion):
     # Guardamos los nombres de todos los archivos en nuetra carpeta de Data Images
     # Ruta
-    PathData=origen
-    
+    PathData = origen
+
     # Lista vacias
     L_nameDataImag = []
     L_nameDataImag = List_name_files(PathData, L_nameDataImag)
-    for i in range(5): #Ejemplo imprimiendo 5 nombres Train
+    for i in range(5):  # Ejemplo imprimiendo 5 nombres Train
         print("Imagen "+str(i)+": "+L_nameDataImag[i])
 
-    # Guardamos en la lista L_RGBData, las matrices de pixeles de cada imagen. 
+    # Guardamos en la lista L_RGBData, las matrices de pixeles de cada imagen.
     # Lista vacia
     L_RGBData = []
     L_RGBData = List_Array_RGB(PathData, L_RGBData)
 
-    for i in range(5):  #Ejemplo imprimiendo 5 Arrays
+    for i in range(5):  # Ejemplo imprimiendo 5 Arrays
         print(str(L_nameDataImag[i])+"--> pixels:"+str(L_RGBData[i].shape))
 
     # Redimensionar las imagenes y las guardamos en la carpeta Data_Images64x64
-    PathData_final=destino
+    PathData_final = destino
     print('Comenzando a redimensionar las imagenes')
-    Redim(PathData,PathData_final,resolucion,resolucion)
+    Redim(PathData, PathData_final, resolucion, resolucion)
     print('Imagenes redimensionadas')
 
     # Ahora que tenemos las imagenes redimensionadas, con el uso de la funcion explicada anteriormente "List_Array_RGB", guardaremos la lista de imagenes redimesionadas.
@@ -75,12 +90,19 @@ def cargar_imagenes(origen, destino, resolucion):
     L_RGBData_final = List_Array_RGB(PathData_final, L_RGBData_final)
     print('Lista de las imagenes guardada')
 
-    for i in range(5):  #Ejemplo imprimiendo 5 Arrays
-        print(str(L_nameDataImag[i])+"--> pixels redim: "+str(L_RGBData_final[i].shape))
+    for i in range(5):  # Ejemplo imprimiendo 5 Arrays
+        print(str(L_nameDataImag[i])+"--> pixels redim: " +
+              str(L_RGBData_final[i].shape))
 
     # Comprobamos las diferencias entre la imagen original y la redimensionada.
-    Plot_Imagenes("Calidad 100% \n",L_nameDataImag,L_RGBData,5)
-    Plot_Imagenes("Redimensionadas \n",L_nameDataImag,L_RGBData_final,5)
+    Plot_Imagenes("Calidad 100% \n", L_nameDataImag, L_RGBData, 5)
+    Plot_Imagenes("Redimensionadas \n", L_nameDataImag, L_RGBData_final, 5)
+
+
+def cargar_imagenes_hilo(PathData_origen, l_name, PathData_destino, resolucion):
+    Redim_Hilo(PathData_origen, l_name,
+               PathData_destino, resolucion, resolucion)
+
 
 def create_folder(path):
     try:
@@ -89,3 +111,46 @@ def create_folder(path):
         print("La creación del directorio %s falló o ya existe" % path)
     else:
         print("Se ha creado el directorio: %s " % path)
+
+
+def comparativa_redim(origen, destino):
+    # Lista vacias
+    L_nameDataImag = []
+    L_nameDataImag = List_name_files(origen, L_nameDataImag)
+    print("Comparariva imagenes redimensionadas y originales:")
+    L_RGBData_final = []
+    L_RGBData_final = List_Array_RGB(destino, L_RGBData_final)
+    L_RGBData = []
+    L_RGBData = List_Array_RGB(origen, L_RGBData_final)
+    Plot_Imagenes("Calidad 100% \n", L_nameDataImag, L_RGBData, 5)
+    Plot_Imagenes("Redimensionadas \n", L_nameDataImag, L_RGBData_final, 5)
+
+
+def save_and_resize(origen, destino, resolucion, num_hilos):
+    """Download and resize a list of images."""
+    # Lista vacias
+    L_nameDataImag = []
+    L_nameDataImag = List_name_files(origen, L_nameDataImag)
+    split_number = int(len(L_nameDataImag)/num_hilos)+1
+    # THREADS
+    split_list = n_slit_list(L_nameDataImag, split_number)
+    Threads_Fabric(origen, destino, split_list,
+                   resolucion, cargar_imagenes_hilo)
+
+
+def n_slit_list(img_list, n):
+    split_img_list = []
+    split_img_list = [img_list[i:i + n]for i in range(0, len(img_list), n)]
+    return split_img_list
+
+
+def Threads_Fabric(origen, destino, img_list, resolucion, fun):
+    threads = []
+    print(len(img_list), "hilos en ejecución...")
+    for i in range(len(img_list)):
+        t = threading.Thread(
+            target=fun, args=(origen, img_list[i], destino, resolucion))
+        threads.append(t)
+        t.start()
+    for i in range(len(threads)):
+        threads[i].join()
--- AA-2/main.py
+++ AA-2/main.py
@@ -1,11 +1,12 @@
 from entrada_salida import normalizar_entradas
-from cargar_imagen import cargar_imagenes, create_folder 
+from cargar_imagen import create_folder, save_and_resize, comparativa_redim
 from datos_csv import cargar_csv
 from tecnicas import dtc, knn, rf, conv, pca, km
 from funcion_aux import cajas, saveToExcel
 from contraste_hipotesis import contraste
 import numpy as np
 import matplotlib.pyplot as plt
+from timeit import timeit
 
 # Crear directorio
 create_folder('dataset/Data_Images64x64')
@@ -13,11 +14,19 @@ create_folder('dataset/Data_Images64x64')
 # Carga las imagenes y las redimensiona a 64x64
 print('Comenzando preprocesado de imagenes.....')
 resolucion = 64
-cargar_imagenes('dataset/Data Images','dataset/Data_Images64x64', resolucion)
+n_hilos = 4
+t1 = timeit("save_and_resize('dataset/Data Images','dataset/Data_Images64x64', resolucion,n_hilos)",
+            globals=globals(), number=1)
+print("Tiempo de ejecución redimension con",
+      str(n_hilos), "hilos: ", str(t1), "s")
 print('Finalizando preprocesado de imagenes.....')
 
+# Comprobamos que cumplen el mismo orden originales y redimensionadas
+comparativa_redim('dataset/Data Images', 'dataset/Data_Images64x64')
+
 print("Comienzo del ordenamiento de datos")
-dato_entrada, t_num = cargar_csv('dataset/Data.csv', 'dataset/Data Images','dataset/Data_Images64x64', {'Food': 0, 'Attire': 1, 'Decorationandsignage':2, 'misc':3})
+dato_entrada, t_num = cargar_csv('dataset/Data.csv', 'dataset/Data Images', 'dataset/Data_Images64x64', {
+                                 'Food': 0, 'Attire': 1, 'Decorationandsignage': 2, 'misc': 3})
 print("Fin del ordenamiento de datos")
 
 print("Normalizar entradas....")
@@ -28,8 +37,9 @@ X_1D_norm, X_3Dnorm = normalizar_entradas(dato_entrada)
 # Se pasan de 12288 elementos a 50
 print("Iniciando reduccion de la dimensionalidad con PCA")
 n_components = 8
-PCA, X_1D_reduced = pca(X = X_1D_norm, n_components = n_components)
-plt.bar(np.arange(1, len(PCA.explained_variance_ratio_) + 1), PCA.explained_variance_ratio_ * 100)
+PCA, X_1D_reduced = pca(X=X_1D_norm, n_components=n_components)
+plt.bar(np.arange(1, len(PCA.explained_variance_ratio_) + 1),
+        PCA.explained_variance_ratio_ * 100)
 plt.show(block=True)
 
 # Clusterizacion mediante el uso de k-medias
@@ -38,8 +48,8 @@ n_clusters = 4
 inercias = []
 
 for n_cluster in range(1, n_clusters + 1):
-    print('Con', n_cluster,'clusters')
-    kmeans = km(X = X_1D_reduced, clusters = n_cluster)
+    print('Con', n_cluster, 'clusters')
+    kmeans = km(X=X_1D_reduced, clusters=n_cluster)
     inercias.append(kmeans.inertia_)
 
 _ = plt.plot(np.arange(1, len(inercias) + 1), inercias)
@@ -49,56 +59,59 @@ plt.show(block=True)
 
 # Entrenamiento DTC
 print("Iniciando entrenamiento DTC")
-CV = np.arange(start = 5, stop = 6, step = 1)
-splits = np.arange(start = 2, stop = 5, step = 1)
-leafs = np.arange(start = 1, stop = 5, step = 1)
+CV = np.arange(start=5, stop=6, step=1)
+splits = np.arange(start=2, stop=5, step=1)
+leafs = np.arange(start=1, stop=5, step=1)
 scoresDTC = dict()
 
 for cv in CV:
     for split in splits:
         for leaf in leafs:
             print('Con CV =', cv, 'y splits =', split, 'y leafs =', leaf)
-            scoresDTC['DTC_CV' + str(cv) + '_splits' + str(split) + '_leafs' + str(leaf)] = dtc(X = X_1D_reduced, Y = t_num, samples_split = split, samples_leaf = leaf, cv = cv)
+            scoresDTC['DTC_CV' + str(cv) + '_splits' + str(split) + '_leafs' + str(leaf)] = dtc(
+                X=X_1D_reduced, Y=t_num, samples_split=split, samples_leaf=leaf, cv=cv)
 
 # Imprimir graficas
-for metric in ['accuracy','roc_auc_ovo','f1_macro', 'precision_macro', 'recall_macro']:
+for metric in ['accuracy', 'roc_auc_ovo', 'f1_macro', 'precision_macro', 'recall_macro']:
     cajas(scoresDTC, metric, 'DTC-' + str(metric))
 
 # Entrenamiento RF
 print("Iniciando entrenamiento RF")
-CV = np.arange(start = 5, stop = 6, step = 1)
-trees = np.arange(start = 5, stop = 101, step = 5)
+CV = np.arange(start=5, stop=6, step=1)
+trees = np.arange(start=5, stop=101, step=5)
 scoresRF = dict()
 
 for cv in CV:
     for tree in trees:
         print('Con CV =', cv, 'y trees =', tree)
-        scoresRF['RF_CV' + str(cv) + '_trees' + str(tree)] = rf(X = X_1D_reduced, Y = t_num, trees = tree, cv = cv)
+        scoresRF['RF_CV' + str(cv) + '_trees' + str(tree)
+                 ] = rf(X=X_1D_reduced, Y=t_num, trees=tree, cv=cv)
 
 # Imprimir graficas
-for metric in ['accuracy','roc_auc_ovo','f1_macro', 'precision_macro', 'recall_macro']:
+for metric in ['accuracy', 'roc_auc_ovo', 'f1_macro', 'precision_macro', 'recall_macro']:
     cajas(scoresRF, metric, 'RF-' + str(metric))
 
 # Entrenamiento KNN
 print("Iniciando entrenamiento KNN")
-CV = np.arange(start = 5, stop = 6, step = 1)
-neighbours = np.arange(start = 5, stop = 101, step = 5)
+CV = np.arange(start=5, stop=6, step=1)
+neighbours = np.arange(start=5, stop=101, step=5)
 scoresKNN = dict()
 
 for cv in CV:
     for neighbour in neighbours:
         print('Con CV =', cv, 'y neighbours =', neighbour)
-        scoresKNN['KNN_CV' + str(cv) + '_neigh' + str(neighbour)] = knn(X = X_1D_reduced, Y = t_num, neighbours = neighbour, cv = cv)
+        scoresKNN['KNN_CV' + str(cv) + '_neigh' + str(neighbour)] = knn(
+            X=X_1D_reduced, Y=t_num, neighbours=neighbour, cv=cv)
 
 # Imprimir graficas
-for metric in ['accuracy','roc_auc_ovo','f1_macro', 'precision_macro', 'recall_macro']:
+for metric in ['accuracy', 'roc_auc_ovo', 'f1_macro', 'precision_macro', 'recall_macro']:
     cajas(scoresKNN, metric, 'KNN-' + str(metric))
 
 # Entrenamiento Keras
 print("Iniciando entrenamiento red convolucional con keras")
-CV = np.arange(start = 5, stop = 6, step = 1)
+CV = np.arange(start=5, stop=6, step=1)
 ler = 0.001
-batch = 64 
+batch = 64
 epochs = 10
 conNeurons = np.array([[8, 16, 16], [16, 16, 16], [16, 32, 32]])
 denseNeurons = np.array([[32, 4], [64, 4], [128, 4]])
@@ -108,12 +121,14 @@ red = 0
 for cv in CV:
     for convolucion in conNeurons:
         for densa in denseNeurons:
-            print('Con CV =', cv, ', conNeurons =', conNeurons, 'y denseNeurons =', denseNeurons)
-            scoresDL['DL_CV' + str(cv) + '_Conv' + str(red)] = conv(X = X_3Dnorm, Y = t_num, cv = cv, conNeurons = convolucion, denseNeurons = densa, ler = ler,  batch = batch, epochs = epochs, resolucion = resolucion)
+            print('Con CV =', cv, ', conNeurons =',
+                  conNeurons, 'y denseNeurons =', denseNeurons)
+            scoresDL['DL_CV' + str(cv) + '_Conv' + str(red)] = conv(X=X_3Dnorm, Y=t_num, cv=cv, conNeurons=convolucion,
+                                                                    denseNeurons=densa, ler=ler,  batch=batch, epochs=epochs, resolucion=resolucion)
             red += 1
 
 # Imprimir graficas
-for metric in ['accuracy','roc_auc_ovo','f1_macro', 'precision_macro', 'recall_macro']:
+for metric in ['accuracy', 'roc_auc_ovo', 'f1_macro', 'precision_macro', 'recall_macro']:
     cajas(scoresDL, metric, 'DL-' + str(metric))
 
 # Grabar a Excel los datos obtenidos para cada tecnica y con la variacion de hiperparametros estudiada
@@ -127,4 +142,5 @@ opcionDTC = 'DTC_CV5_splits3_leafs3'
 opcionKNN = 'KNN_CV5_neigh100'
 opcionRF = 'RF_CV5_trees80'
 opcionDL = 'DL_CV5_Conv8'
-contraste(accuracy1 = scoresDTC[opcionDTC]['accuracy'], accuracy2 = scoresKNN[opcionKNN]['accuracy'], accuracy3 = scoresRF[opcionRF]['accuracy'], accuracy4 = scoresDL[opcionDL]['accuracy'], alpha = 0.1)
+contraste(accuracy1=scoresDTC[opcionDTC]['accuracy'], accuracy2=scoresKNN[opcionKNN]['accuracy'],
+          accuracy3=scoresRF[opcionRF]['accuracy'], accuracy4=scoresDL[opcionDL]['accuracy'], alpha=0.1)
